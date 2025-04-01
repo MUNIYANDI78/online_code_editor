75d0c8d881a60e29924e93d8a37423046a1865b0
mtcreation <mt9318832@gmail.com.com>
2025-04-02 05:28:10 +0530

    Fixed .gitignore issues

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from "../images/logos/logo.png"
+import logo from "../images/logos/logo2.png"
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
